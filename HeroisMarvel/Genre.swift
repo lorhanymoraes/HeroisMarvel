a472e6bda18fff429566c0f7525becea09754fe7
lorhanymoraes <107012585+lorhanymoraes@users.noreply.github.com>
2022-11-10 22:12:59 -0300

    app pronto

---
+++ HeroisMarvel/Genre.swift
@@ -0,0 +1,17 @@
+//
+//  Genre.swift
+//  HeroisMarvel
+//
+//  Created by Lorhany Moraes on 14/07/22.
+//  Copyright © 2022 Eric Brito. All rights reserved.
+//
+
+import Foundation
+
+struct Genre {
+    let id: Int
+    let name: String
+    
+    var genreId: [Genre] = []
+    
+}
---
+++ HeroisMarvel/MarvelAPI.swift
@@ -0,0 +1,52 @@
+//
+//  MarvelAPI.swift
+//  HeroisMarvel
+//
+//  Created by Lorhany Moraes on 28/06/22.
+//  Copyright © 2022 Eric Brito. All rights reserved.
+//
+
+import Foundation
+import SwiftHash
+import Alamofire
+
+class MarvelAPI {
+    static private let basePath = "https://gateway.marvel.com/v1/public/characters?"
+    static private let privateKey = "7dbda0ac4053185071d63f390353cb968e49c65b"
+    static private let publicKey = "6c203a316e8d0fcea085e3443b3129fa"
+    static private let limit = 50
+    
+    class func loadHeros(name: String?, page: Int = 0, onComplete: @escaping (MarvelInfo?) -> Void) {
+        let offset = page * limit
+        let startsWith: String
+        if let name = name, !name.isEmpty {
+            startsWith = "nameStartsWith=\(name.replacingOccurrences(of: " ", with: ""))&"
+        } else {
+            startsWith = ""
+        }
+        let url = basePath + "offset=\(offset)&limit=\(limit)&" + startsWith + getCredentials()
+        print(url)
+        
+        AF.request(url).responseJSON { (response) in
+            guard let data = response.data else {
+                onComplete(nil)
+                return
+            }
+            do {
+                let marvelInfo = try JSONDecoder().decode(MarvelInfo.self, from: data)
+                onComplete(marvelInfo)
+            } catch {
+                print(error.localizedDescription)
+                onComplete(nil)
+            }
+        
+            } 
+    }
+    
+    private class func getCredentials() -> String {
+        let ts = String(Date().timeIntervalSince1970)
+        let hash = MD5(ts+privateKey+publicKey).lowercased()
+        return "ts=\(ts)&apikey=\(publicKey)&hash=\(hash)"
+    }
+    
+}
